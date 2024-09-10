7b661adce0247cf53582f367b651842ef91aa2f4
De Pembroke Alexandre <alex.depem@gmail.com>
2024-09-10 16:44:09 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  productionSourceMap: false,
+  configureWebpack: {
+    output: {
+      libraryExport: 'default',
+    },
+  },
+}
